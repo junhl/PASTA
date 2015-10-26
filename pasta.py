40dd8503a41ab02566cc1880e24fb9ee11745709
Jun Ho Lee <jun.h.lee@mail.mcgill.ca>
2015-10-26 02:14:34 -0400

    initial push

---
+++ pasta.py
@@ -0,0 +1,63 @@
+# Authors  Jun Ho Lee, Colby Simpson
+# PASTA - Probabilistic Alignment Search Tool Advance
+
+import collections
+import sys
+import os
+
+nucleotides = ['A','C','G','T']
+
+# a helper function that may be useful in later on. 
+# input : sequence from datafile from commandline
+# output : probabilistic seq/matrix
+# this is also to show Colby the structure of a dictionary
+# usually I just use standard dictionary but it is saved in non-ordered way. Just to show you, using an ordered dictionary
+def parse_seq(data):
+	prob_seq = collections.OrderedDict()
+	with open(data) as f:
+		for seq in f:
+			seq = seq.strip()
+			for nuc in nucleotides:
+				for i in range(0,len(seq)):
+					if nuc == seq[i]:
+						prob_seq[nuc,i] = 1
+					else:
+						prob_seq[nuc,i] = 0
+		#print prob_seq
+		return prob_seq
+
+# help for general users. To be expanded as we add more parameters
+def help():
+  print """
+  Required:
+    -d <file_path> 
+      A file containing the query sequence
+
+  e.g.
+    python pasta.py -d query.txt
+    """
+
+# this is like main in Java	
+if __name__ == "__main__":
+	param = sys.argv
+	l = len(param)
+	i = 1
+	
+	while i < l:
+		options = param[i]
+		if not options.startswith('-'):
+			i += 1
+		else:
+			if options == '-d':  
+				file_name = param[i+1]
+				i += 2
+			if not os.path.isfile(file_name):
+				print "File not found !"
+				help()
+				sys.exit(1)
+	try:			
+		parse_seq(file_name)
+	except NameError:
+		print "Need to specify a file !"
+		help()
+		sys.exit(1)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+PASTA - Probabilistic Alignment Search Tool Advance
+Written by Jun Ho Lee and Colby Simpson
+
